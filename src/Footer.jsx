296ce8b04bda4cc608529a3d2909e04905ef4cc1
Francis <oshifrancis2@gmail.com>
2023-07-12 11:46:01 -0700

    updated

---
+++ src/Footer.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+const Footer = () => {
+  return (
+<>
+{/*start of mobile footer*/}
+<footerf>
+          <div className="footercontent1"><br /><br />
+            <h2>Newsletter</h2>
+            <form1>
+              <input type="email" placeholder="Type Email Address" /><br /><br /><br />
+              <input type="submit" className="submit" defaultValue="subscribe" />
+            </form1>
+          </div>
+          <div className="footercontent23">
+            <div className="footercontent2">
+              <h2>Join BlueCollar</h2>
+              <a style={{marginTop: '-40px'}}>For BlueCollar technology</a><br />
+              <a>Learning Community</a><br />
+              <a>Leadership Program</a><br />
+              <a>Become a Partner</a>
+            </div>
+            <div className="footercontent3">
+              <h2>Services</h2>
+              <a>Electrical</a><br />
+              <a>Company</a><br />
+              <a>Plumbering</a><br />
+              <a>Electronics &amp; Gadgets</a>
+            </div></div>
+          <div className="footercontent4">
+            <h2>About Us</h2>
+            <a>Who are we</a><br />
+            <a>Blogs</a><br />
+            <a>Events</a><br />
+            <a>Projects</a>
+          </div><br />
+          <a className="fa fa-facebook" />
+          <a className="fa fa-linkedin" />
+          <a className="fa fa-twitter" />
+          <a className="fa fa-instagram" />
+        </footerf>
+        {/*end of mobile footer*/}
+        {/*start of footer*/}
+        <footer>
+          <div className="leftfooter">
+            <div className="textonbottom">Blue Collar</div>
+            <div className="socialsonfooter">
+              <a className="fa fa-facebook" />
+              <a className="fa fa-linkedin" />
+              <a className="fa fa-twitter" />
+              <a className="fa fa-instagram" /></div>
+          </div><hr />
+          <div className="rightfooter">
+            <h2>Want to know what we are up to?</h2>
+            <div className="form1">
+              <input type="email" placeholder="Type Email Address" />
+              <button7 type="submit">Subscribe</button7>
+            </div>
+            <div className="rightfooterlist">
+              <div className="footercontent2">
+                <h2>Join BlueCollar</h2>
+                <a>For Blue Collar technology</a><br />
+                <a>Learning Community</a><br />
+                <a>Leadership Program</a><br />
+                <a>Become a Partner</a>
+              </div>
+              <div className="footercontent3">
+                <h2>Services</h2>
+                <a>Electrical</a><br />
+                <a>Company</a><br />
+                <a>Plumbering</a><br />
+                <a>Electronics &amp; Gadgets</a>
+              </div>
+              <div className="footercontent4">
+                <h2>About Us</h2>
+                <a>Who are we</a><br />
+                <a>Blogs</a><br />
+                <a>Events</a><br />
+                <a>Projects</a>
+              </div></div>
+          </div>
+        </footer>
+        {/*end of footer*/}
+
+</>
+
+  )
+}
+
+export default Footer;
---
+++ src/Section.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import playcircle from './images/playcircle.svg';
+import onk1 from './images/onk1.svg';
+
+
+
+const Section = () => {
+  return (
+<>
+        {/*section*/}
+        <div className="section">
+          <div className="sectioncontent">
+            <div className="sectioncontent1">
+              <h1>
+                The work is on us<br />
+                The focus is you
+              </h1>
+            </div>       
+            <div className="sectioncontent2">
+              <h2>Skilled, vetted, tested and qualified technical talents for your construction, maintenance, installation, repairs, renovation and verification services for</h2>
+              <div className="banner">
+                <div className="banner-text">
+                  <div className="text-wrapper">
+                    <div className="box b1" style={{color: '#E6D0C4'}}>Construction Agencies</div>
+                    <div className="box b2" style={{color: '#E8D1CB'}}>Real Estate Development</div>
+                    <div className="box b3" style={{color: '#D9E2DD'}}>Startups &amp; Organization</div>
+                    <div className="box b4" style={{color: '#DAD2D3'}}>Home Owners</div>
+                    <div className="box b5" style={{color: '#E6DFEF'}}>Hospitality &amp; Resort Agencies</div>
+                  </div></div></div>
+            </div>
+            <div className="textonbottomofhomescreen">
+              <a href=" waitinglist.html"><div className="button1">I WANT A TECHNICIAN NOW &gt;</div></a>
+              <a href="waitinglist.html"><div className="textonbottomofhomescreen1">
+                  <div className="text2"><img src={playcircle} /></div>
+                  <div className="button2">See the new offer for homeowners</div>
+                  <div className="message"><img src={onk1}height="39.75757598876953px;" width="32px;" left="316px;" top="512.121337890625px;" border-radius="0px;" style={{position: 'fixed', marginLeft: '16px', marginTop: '13px'}} /></div>
+                </div></a></div></div></div>
+        {/*end of section*/}
+</>
+  )
+}
+
+export default Section;
