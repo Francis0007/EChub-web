5187ab2cd711c94c9aaac3089ca93b2a23fecd5a
Francis <oshifrancis2@gmail.com>
2023-07-17 05:52:26 -0700

    updated

--- src/Section.jsx
+++ src/Section.jsx
@@ -10,7 +10,7 @@ const Section = () => {
 <>
         {/*section*/}
         <div className="section">
-          <div className="sectioncontent">
+          <div className="sectioncontent"><br /><br /><br /><br /><br /><br /><br /><br /><br />
             <div className="sectioncontent1">
               <h1>
                 The work is on us 
@@ -18,23 +18,22 @@ const Section = () => {
               </h1>
             </div>       
             <div className="sectioncontent2">
-              <h2>Tap into a pool of skilled, vetted, tested and qualified technical talents for your </h2>
-              <div className="banner">
-                <div className="banner-text">
+              <h2>Tap into a pool of skilled, vetted, tested<br /> and qualified technical talents for your </h2>
+
                   <div className="text-wrapper">
                     <div className="box b1" style={{color: '#E6D0C4'}}>Construction Agencies</div>
                     <div className="box b2" style={{color: '#E8D1CB'}}>Real Estate Development</div>
                     <div className="box b3" style={{color: '#D9E2DD'}}>Startups &amp; Organization</div>
                     <div className="box b4" style={{color: '#DAD2D3'}}>Home Owners</div>
                     <div className="box b5" style={{color: '#E6DFEF'}}>Hospitality &amp; Resort Agencies</div>
-                  </div></div></div>
+                  </div>
             </div>
             <div className="textonbottomofhomescreen">
               <a href=" waitinglist.html"><div className="button1">I WANT A TECHNICIAN NOW &gt;</div></a>
               <a href="waitinglist.html"><div className="textonbottomofhomescreen1">
                   <div className="text2"><img src={playcircle} /></div>
                   <div className="button2">Our vision for you</div>
-                  <div className="message"><img src={onk1}height="39.75757598876953px;" width="32px;" left="316px;" top="512.121337890625px;" border-radius="0px;" style={{position: 'fixed', marginLeft: '16px', marginTop: '13px'}} /></div>
+                  <div className="message"><img src={onk1}height="39.75757598876953px;" width="32px;" left="16px;" top="512.121337890625px;" border-radius="0px;" style={{position: 'fixed', marginLeft: '-15px', marginTop: '13px'}} /></div>
                 </div></a></div></div></div>
         {/*end of section*/}
 </>
