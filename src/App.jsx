387d6fa168db841360e514a22e07b45211d0b5e1
Francis <oshifrancis2@gmail.com>
2023-07-22 08:16:26 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,9 @@ function App() {
         <Section />
         <Section1 />
         <Section2 />
-        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Footer />
     </>
   )
 }
