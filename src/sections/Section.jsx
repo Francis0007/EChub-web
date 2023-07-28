9246ee454eb7ac54b69523ffd1f5a21c38203749
Francis <oshifrancis2@gmail.com>
2023-07-28 15:18:25 -0700

    updated

--- src/sections/Section.jsx
+++ src/sections/Section.jsx
@@ -8,6 +8,7 @@ import Section3 from './Section3.jsx';
 import Section4 from './Section4.jsx';
 import Section5 from './Section5.jsx';
 import Footer from './Footer.jsx';
+import { Link } from 'react-router-dom';
 
 
 
@@ -33,12 +34,12 @@ const Section = () => {
                   </div>
             </div>
             <div className="textonbottomofhomescreen">
-              <a href=" waitinglist.html"><div className="button1">I WANT A TECHNICIAN NOW &gt;</div></a>
-              <a href="waitinglist.html"><div className="textonbottomofhomescreen1">
+              <Link to='./waitlist'><div className="button1">I WANT A TECHNICIAN NOW &gt;</div></Link>
+              <Link to='./waitlist'><div className="textonbottomofhomescreen1">
                   <div className="text2"><img src={playcircle} /></div>
                   <div className="button2">Our vision for you</div>
                   <div className="message"><img src={onk1}height="39.75757598876953px;" width="32px;" left="16px;" top="512.121337890625px;" border-radius="0px;" style={{position: 'fixed', marginLeft: '-15px', marginTop: '13px'}} /></div>
-                </div></a></div></div></div>
+                </div></Link></div></div></div>
         {/*end of section*/}
         <Section1 />
         <Section2 />
