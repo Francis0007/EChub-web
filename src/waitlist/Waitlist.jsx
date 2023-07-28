3b2ead6ab9c34f8fc66ccbb55db14755a8356946
Francis <oshifrancis2@gmail.com>
2023-07-28 06:56:56 -0700

    updated

--- src/waitlist/Waitlist.jsx
+++ src/waitlist/Waitlist.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Waitlist.css';
+import './waitlist.css';
 import { Link } from 'react-router-dom';
 
 
@@ -7,12 +7,30 @@ import { Link } from 'react-router-dom';
 const Waitlist = () => {
   return (
 <>
-<div className="section7"><br />
-  <h1>Get early access<br />
-          by joining our waitlist</h1>
-          <p>Once you are in, you can climb the queue by inviting your friends.</p>
-          <p>The closer to the top, the sooner you'll get the access</p><br />
-          <center><Link to='/waitlistform'><div className="btn">STEP INSIDE</div></Link></center><br /><br /><br /><br />
+<div className='section7'><br />
+<div className='sec'>
+  <h1 id="showcase">*Join the EChub waitlist*</h1>
+          <p id="content">Are you ready to revolutionize the way you manage your facilities?</p>
+          <p id="content">At EChub, we are excited to introduce our tech-driven platform thet empowers you to take complete control of your facilitie's
+           maintenance and repairs. Say goodbye to the hassle of tracking
+           maintenance schedules, project report, and facility manually.<br></br> With EChub, everything you need is a click away </p>
+           <h1 id="showcase">*Why join the Waitlist?*</h1>
+         <p id="content1">- Get early access: Be among the first to experience the power of EChub as soon as it launches!</p>
+         <p id="content1">- Stay Updated: Receive exclusive updates, news and insights about our platform's development progress.</p>
+         <h1 id="showcase">*How it Works*</h1>
+          <p id="content2">1. Join the Waitlist: Enter your email below to join the waitlist. it's quick and easy!</p>
+          <p id="content2">2. Be in the Know: We'll keep you informed about our platform's development and launch</p>
+          <p id="content2">3. Get Ready to Transform: Once we launch, you'll have access to EChub game-changing features.</p>
+         <h1 id="showcase">*Join the Revolution*</h1>
+          <Link to='/waitlistform'><div className="btn">STEP INSIDE</div></Link>
+          <a className="fa fa-facebook" />
+              <a className="fa fa-linkedin" />
+              <a className="fa fa-twitter" />
+              <a className="fa fa-instagram" />
+              <p>Follow us on social media for exciting updates and sneak peeks!</p>
+              <p>© 2023 EChub. All rights reserved.</p>
+              <p>At EChub we believe nobody deserves to see shege from local technicians</p>
+        </div>
         </div>
 </>
   )
--- src/waitlist/Waitlistform.jsx
+++ src/waitlist/Waitlistform.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import './Waitlist.css';
+import './waitlist.css';
 
 
 
 const Waitlistform = () => {
   return (
 <>
-<h3>Waitlistform</h3>
+<div className='section8'>
+  <h1>Please fill this form</h1><br />
+
+  <center>
+    <form action="https://formspree.io/f/xwkdvkbg"
+  method="POST"> 
+  <input type='email' name='Email Address' placeholder='Email Address'requird/> 
+  <input type='text' name='Full Name' placeholder='Full Name' required/>
+  <input type='number' name='Phone Number' placeholder='Phone Number' required/>
+  <input type='text' name='Name of Business' placeholder='Name of Business(if not signing as a homeowner)' /><br />
+<textarea type='text' name='What You are expecting to see' placeholder='What You are expecting to see' /><br />
+<input type="submit" /><br /><br /><br />
+  </form>  
+  </center>
+
+
+</div>
 </>
   )
 }
